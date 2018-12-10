2a2b8b10109d4abb0fcec5f5119dc58ff10ab996
peteryxu <peteryxu2012@gmail.com>
2018-12-09 21:51:35 -0500

    Initial Commit

---
+++ BackendDataExchange/BackendDataExchange.h
@@ -0,0 +1,19 @@
+//
+//  BackendDataExchange.h
+//  BackendDataExchange
+//
+//  Created by Peter Xu on 12/9/18.
+//  Copyright © 2018 We Sense Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BackendDataExchange.
+FOUNDATION_EXPORT double BackendDataExchangeVersionNumber;
+
+//! Project version string for BackendDataExchange.
+FOUNDATION_EXPORT const unsigned char BackendDataExchangeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BackendDataExchange/PublicHeader.h>
+
+
